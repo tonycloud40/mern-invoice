232587495fcd5c68344324f06b6fc1aaf9e3199d
Tony Cloud <tonycloud40@gmal.com>
2023-05-30 18:22:14 -0400

    Day3: added logger, fixed docker,  winston  pkg

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import chalk from 'chalk'
 import morgan from 'morgan'
 import express from 'express';
 
+import {morganMiddleware, systemLogs} from "./utils/Logger.js"
+
 
 const app = express();
 
@@ -15,6 +17,8 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+app.use(morganMiddleware)
+
 app.get("/api/v1/test", (req, res) => {
     res.json({Hi : "Welcome to the Invoice App"});
 });
@@ -22,6 +26,14 @@ app.get("/api/v1/test", (req, res) => {
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`${chalk.green.bold("✓")} 👍 Server running in ${chalk.yellow.bold(process.env.NODE_ENV)}
-    mode on port ${chalk.blue.bold(PORT)}`);
+    const message = `${chalk.green.bold("✓")} 👍 \
+    Server running in ${chalk.yellow.bold(process.env.NODE_ENV)} \
+    mode on port ${chalk.blue.bold(PORT)}`;
+    
+    //Display in the console
+    console.log(message);
+
+    // Add to the Log
+    systemLogs.info(message);
+
 });
